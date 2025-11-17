0ca2399d66a89d52822fbd242ea96df00556beb7
GuilhermeDellatin <guidellatin@gmail.com>
2025-11-17 08:39:43 -0300

    Some instructions decorators and iterators challenge

--- luiza_labs_backend_python/fundamentos/desafio_01.py
+++ luiza_labs_backend_python/fundamentos/desafio_01.py
@@ -255,9 +255,16 @@ def listar_contas(contas: List[Dict]):
 # Decorador de log
 # Implemente um decorador que seja aplicado a todas as funções de
 # transações (depósito, saque, criação de conta, etc). Esse decorador
-# deve registrar (printar) a data e hora de cada transação, bem como o tipo de transação
+# deve registrar (printar) a data e hora de cada transação, bem como o tipo de transação.
 
+# Gerador de relatórios
+# Criar um gerador que permita iterar sobre as transações de uma conta e retorne, uma a uma,
+# as transações que foram realizadas. Esse gerador deve também ter uma forma de filtrar as
+# transações baseado em seu tipo (por exemplo, apenas saques ou apenas depósitos).
 
+# Iterador personalizado
+# Implemente um iterador personalizado ContaIterador que permita iterar sobre todas as contas
+# de banco, retornando informações básicas de cada conta (número, saldo atual, etc).
 
 def main():
     saldo = 0.0
