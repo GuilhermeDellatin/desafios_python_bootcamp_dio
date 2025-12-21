729baae5c001351155e1d9d5858573681a7a5595
GuilhermeDellatin <guidellatin@gmail.com>
2025-12-21 18:16:13 -0300

    OOP challenge create deposit and withdraw.py class

--- luiza_labs_backend_python/oop/challenge/transaction.py
+++ luiza_labs_backend_python/oop/challenge/transaction.py
@@ -10,3 +10,29 @@ class Transaction(ABC):
     @classmethod
     def register(cls, bank_account):
         pass
+
+class Deposit(Transaction):
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def register(self, bank_account):
+        success = bank_account.deposit(self._value)
+        if success:
+            bank_account.history.add_transaction(self)
+
+class Withdraw(Transaction):
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def register(self, bank_account):
+        success = bank_account.withdraw(self._value)
+        if success:
+            bank_account.history.add_transaction(self)
